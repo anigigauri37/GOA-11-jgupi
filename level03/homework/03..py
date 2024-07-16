dcb174fe0360580b490eaf9702afcd0c2b7b766d
anigigauri37 <anigigauri37@gmail.com>
2024-07-16 17:33:16 +0400

    first commir

---
+++ level03/homework/03..py
@@ -0,0 +1,13 @@
+#შექმენით მეორე ფაილი სადაც გაამწარებთ იმ მიმის სახეს
+
+p rint (an ni, gigau rr i, 1 4 wlis"
+pri nt 45)))
+num=int(input("enter ")
+num = in t(input"ent )
+inb =ann i"(*)
+prnt 15gamarjoba
+print("father 
+prin t (type,)name)
+(name  " sur name + " hoby + " country()
+(heht /* anni")
+
---
+++ level04/homework/04.py
@@ -0,0 +1,8 @@
+#თქვენი ოჯახის წევრების ასაკები ჩამოწერეთ ცვლადების სახით
+
+name = "eka"
+age = "33"
+name1 = "giorgi"
+age1 = "33"
+
+print(name, age, name1, age1)
